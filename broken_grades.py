397383d2a087c5bcb2a2f494b85974bb30586959
merli <aa6031@live.mdx.ac.uk>
2025-03-02 18:01:15 +0400

    Converted input() to int(input()) Renamed exam_3 to exam_three Changed str() to int() Fixed syntax error in grades list Changed grade to grades Changed grdes to grades Added colon in elif statement Simplified 'C' and 'D' grades' elif statements Fixed quotes in 'C' grade Changed elif to else Removed for loop Changed grade to grades Fixed syntax for 'F' grade in if statement Added round brackets in print statement

--- broken_grades.py
+++ broken_grades.py
@@ -21,35 +21,35 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = int(input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: ")) #Angeline converted input() to int(input())
 
-exam_three = int(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: ")) #Angeline renamed exam_3 to exam_three and changed str() to int()
 
-grades = [exam_one,exam_two,exam_three]
+grades = [exam_one,exam_two,exam_three] #Angeline fixed syntax error in grades list
 sum = 0
-for grade in grades:
+for grade in grades: # Angeline changed grade to grades
   sum = sum + grade
 
-avg = sum / len(grades)
+avg = sum / len(grades) #Angeline changed grdes to grades
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90:
+elif avg >= 80 and avg < 90: #Angeline added colon in elif statement
     letter_grade = "B"
-elif avg >= 70 and avg < 80:
-    letter_grade = "C"
-elif avg >= 60 and avg <70:
+elif avg >= 70 and avg < 80: #Angeline simplified 'C' grade elif statement
+    letter_grade = "C" #Angeline fixed quotes in 'C' grade
+elif avg >= 60 and avg <70: #Angeline simplified 'D' grade elif statement
     letter_grade = "D"
-else:
+else: #Angeline changed elif to else
     letter_grade = "F"
 
-print("Exam: " + str(grades))
+print("Exam: " + str(grades)) #Angeline removed for loop and changed grade to grades
 
 print("Average: " + str(avg))
 
 print("Grade: " + letter_grade)
 
-if letter_grade == "F":
-    print("Student is failing.")
+if letter_grade == "F": #Angeline fixed syntax for 'F' grade in if statement
+    print("Student is failing.") #Angeline added round brackets in print statement
 else:
-    print("Student is passing.")
+    print("Student is passing.") #Angeline added round brackets in print statement
