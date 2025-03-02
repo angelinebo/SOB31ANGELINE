f8c004131197da999a3af5b111f000571b8f6662
merli <aa6031@live.mdx.ac.uk>
2025-03-02 21:04:19 +0400

    assigned variables and added for loop added round brackets in print statement changed raw_input() to input() modified the print statement

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,13 +1,21 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+    cows = 0 #Angeline assigned variables and added for loop
+    bulls = 0
 
-playing = True #gotta play the game
+    for i in range(len(number)):
+        if user_guess[i] == number[i]:
+            bulls += 1
+        elif user_guess[i] in number:
+            cows += 1
+
+    return cows, bulls
+
+playing = True #got to play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+print(number) #Angeline added round brackets in print statement
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +24,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!") #Angeline changed raw_input() to input()
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
@@ -26,7 +34,7 @@ while playing:
 
     if cowbullcount[1]==4:
         playing = False
-        print("You win the game after " + str(guesses) + "! The number was "+str(number))
+        print("You win the game after " + str(guesses) + " guesses! The number was "+str(number)) #Angeline modified the print statement
         break #redundant exit
     else:
         print("Your guess isn't quite right, try again.")
