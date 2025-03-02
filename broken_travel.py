f2cef71481e5b7ccc2744697687b4e5bb2dae669
merli <aa6031@live.mdx.ac.uk>
2025-03-02 18:44:47 +0400

    Fixed input() syntax for year variable Added colon in if statement Changed single quote to double quotes Changed '&&' to 'and' Changed elif to else

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year = int(input("Greetings! What is your year of origin? "))
+year = int(input("Greetings! What is your year of origin? ")) #Angeline fixed input() syntax for year variable
 
-if year <= 1900:
-    print ("Woah, that's the past!")
-elif year > 1900 and year < 2020:
+if year <= 1900: #Angeline added colon in if statement
+    print ("Woah, that's the past!") #Angeline changed single quote to double quotes
+elif year > 1900 and year < 2020: #Angeline changed '&&' to 'and'
     print ("That's totally the present!")
-else:
+else: #Angeline changed elif to else
     print ("Far out, that's the future!!")
